ce76fb479fecc95e9d37450a0f810ca6e7b1fe44
raki <rakeshhotker@gmail.com>
2021-01-13 21:31:00 +0530

    added css

--- src/components/App.js
+++ src/components/App.js
@@ -1,5 +1,7 @@
 import React from "react";
 import { data } from "../data";
+import Navbar from "./Navbar";
+import MovieCard from "./MovieCard";
 function App() {
   return (
     <div className="App">
---
+++ src/components/Navbar.js
@@ -0,0 +1,16 @@
+import { render } from "@testing-library/react";
+import React from "react";
+class Navbar extends React.Component {
+  render() {
+    return (
+      <div className="nav">
+        <div className="search-container">
+          <input />
+          <button id="search-button">Search</button>
+        </div>
+      </div>
+    );
+  }
+}
+
+export default Navbar;
