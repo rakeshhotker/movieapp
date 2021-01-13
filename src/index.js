0b1da9f902b06bd45d6ed1b2adf01cb5eab13706
raki <rakeshhotker@gmail.com>
2021-01-14 04:01:46 +0530

    actions and reducers folders created

--- src/index.js
+++ src/index.js
@@ -2,6 +2,8 @@ import React from "react";
 import ReactDOM from "react-dom";
 import "./index.css";
 import App from "./components/App";
+import LogRocket from "logrocket";
+LogRocket.init("3i2v1t/movieapp");
 
 ReactDOM.render(
   <React.StrictMode>
