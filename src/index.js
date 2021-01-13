ddf347b63fdf8e45921a18399d4b147a770a2554
raki <rakeshhotker@gmail.com>
2021-01-14 04:24:56 +0530

    few more changes

--- src/index.js
+++ src/index.js
@@ -1,10 +1,15 @@
 import React from "react";
 import ReactDOM from "react-dom";
+import { createStore } from "redux";
 import "./index.css";
 import App from "./components/App";
-import LogRocket from "logrocket";
-LogRocket.init("3i2v1t/movieapp");
+import movies from "./reducers";
+//import LogRocket from "logrocket";
+//LogRocket.init("3i2v1t/movieapp");
 
+const store = createStore(movies);
+console.log("store", store);
+console.log("getState", store.getState());
 ReactDOM.render(
   <React.StrictMode>
     <App />
