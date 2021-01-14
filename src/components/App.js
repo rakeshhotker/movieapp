387069299f16e70436dc2bb2de209034e674ee38
raki <rakeshhotker@gmail.com>
2021-01-14 16:14:38 +0530

    subscribe method in use

--- src/components/App.js
+++ src/components/App.js
@@ -2,23 +2,39 @@ import React from "react";
 import { data } from "../data";
 import Navbar from "./Navbar";
 import MovieCard from "./MovieCard";
-function App() {
-  return (
-    <div className="App">
-      <Navbar />
-      <div className="main">
-        <div className="tabs">
-          <div className="tab">Movies</div>
-          <div className="tab">Favourites</div>
+class App extends React.Component {
+  componentDidMount() {
+    const { store } = this.props;
+    store.subscribe(() => {
+      this.forceUpdate(); //never use this method to update unless testing
+    });
+    //make api call
+    //dispatch action
+    store.dispatch({
+      type: "ADD_MOVIES",
+      movies: data,
+    });
+    console.log("state", this.props.store.getState());
+  }
+  render() {
+    const movies = this.props.store.getState();
+    return (
+      <div className="App">
+        <Navbar />
+        <div className="main">
+          <div className="tabs">
+            <div className="tab">Movies</div>
+            <div className="tab">Favourites</div>
+          </div>
+        </div>
+        <div className="list">
+          {movies.map((movie, index) => (
+            <MovieCard movie={movie} key={`movies-${index}`} />
+          ))}
         </div>
       </div>
-      <div className="list">
-        {data.map((movie, index) => (
-          <MovieCard movie={movie} key={`movies-${index}`} />
-        ))}
-      </div>
-    </div>
-  );
+    );
+  }
 }
 
 export default App;
